aee52648532b3399605e0f1fdd8e5a270131d36d
vsinky <sinkykumariverma@gmail.com>
2021-11-26 22:38:14 +0530

    Added:Code for search value

--- src/com/bridgelabz/binary/search/BinaryTreeMain.java
+++ src/com/bridgelabz/binary/search/BinaryTreeMain.java
@@ -3,28 +3,32 @@ package com.bridgelabz.binary.search;
 public class BinaryTreeMain {
 
 	public static void main(String[] args) {
+
 			MyBinaryTree<Integer> binaryTree = new MyBinaryTree<>();
 			binaryTree.add(56);
 
 			binaryTree.add(30);
 			binaryTree.add(70);
-			
+
 			binaryTree.add(22);
 			binaryTree.add(40);
 			binaryTree.add(60);
-			
+
 			binaryTree.add(95);
 			binaryTree.add(11);
 			binaryTree.add(65);
-			
+
 			binaryTree.add(3);
 			binaryTree.add(16);
 			binaryTree.add(63);
-			
+
 			binaryTree.add(67);
 			binaryTree.inOrder();
 			int size = binaryTree.getSize();
-
-			System.out.print("Size of tree is " + size);
+	
+			System.out.println("Size of tree is " + size);
+			Boolean check = binaryTree.search(63);
+		
+			System.out.print("63 -> " + check);
 		}
 	}
--- src/com/bridgelabz/binary/search/MyBinaryTree.java
+++ src/com/bridgelabz/binary/search/MyBinaryTree.java
@@ -1,22 +1,23 @@
 package com.bridgelabz.binary.search;
 
 public class MyBinaryTree<K extends Comparable<K>> {
-private MyBinaryNode<K> root;
-	
+	private MyBinaryNode<K> root;
+
 	public void add(K key) {
 		this.root = this.addRecursive(root, key);
 	}
 
 	private MyBinaryNode<K> addRecursive(MyBinaryNode<K> root, K key) {
-		
-		if(root == null) {
+
+		if (root == null) {
 			return new MyBinaryNode<>(key);
 		}
 		int comRes = key.compareTo(root.key);
-		if(comRes == 0) return root;
-		if(comRes < 0) {
+		if (comRes == 0)
+			return root;
+		if (comRes < 0) {
 			root.left = addRecursive(root.left, key);
-		}else {
+		} else {
 			root.right = addRecursive(root.right, key);
 		}
 		return root;
@@ -27,18 +28,37 @@ private MyBinaryNode<K> root;
 	}
 
 	private int getSizeRecursive(MyBinaryNode<K> root) {
-	
-		return root == null ?0 : 1+this.getSizeRecursive(root.left)+this.getSizeRecursive(root.right);
+
+		return root == null ? 0 : 1 + this.getSizeRecursive(root.left) + this.getSizeRecursive(root.right);
 	}
+
 	public void inOrder() {
 		inOrderRec(this.root);
 	}
+
 	private void inOrderRec(MyBinaryNode<K> root) {
-		
-		if(root != null) {
+
+		if (root != null) {
 			inOrderRec(root.left);
 			System.out.println(root.key);
 			inOrderRec(root.right);
 		}
 	}
-}
+
+	public Boolean search(K key) {
+		return searchRecursive(root, key);
+	}
+
+	private Boolean searchRecursive(MyBinaryNode<K> current, K key) {
+		if (current == null) {
+			return false;
+		}
+		if (key == current.key) {
+			return true;
+		}
+		if (key.compareTo(current.key)<0)
+			return searchRecursive(current.left, key);
+		else
+			return searchRecursive(current.right, key);
+	}
+	}
